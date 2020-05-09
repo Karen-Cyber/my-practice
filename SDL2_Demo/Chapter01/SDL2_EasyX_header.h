82de28b327752564fcb914ed8ea0ba2f0487cb30
Karen-Cyber <58882733+Karen-Cyber@users.noreply.github.com>
2020-05-09 17:13:44 +0800

    Game Programming In C++ 01

---
+++ SDL2_Demo/Chapter01/SDL2_EasyX_header.h
@@ -0,0 +1,42 @@
+#ifndef _GAME_H_
+#define _GAME_H_
+#include <SDL.h>
+
+struct vector2
+{
+	float x;
+	float y;
+};
+
+class GAME
+{
+private:
+	void ProcessInput();
+	void UpdateGame();
+	void Generateoutput();
+	SDL_Window* mWindow;
+	SDL_Renderer* mRenderer;
+	bool mIsRunning;
+
+	static const int mWidth = 800;
+	static const int mHeight = 600;
+	static const int mthickness = 15;
+
+	vector2 mPaddlePos;
+	vector2 mPaddlePos2;
+	vector2 mBallPos;
+	vector2 mBallVel;
+
+	Uint32 mTicksCount;
+
+	int mPaddleDir;
+	int mPaddleDir2;
+	int paddleH;
+
+public:
+	GAME();
+	bool Initialize();
+	void RunLoop();
+	void ShutDown();
+};
+#endif
